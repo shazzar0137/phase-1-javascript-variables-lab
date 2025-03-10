19424574751d2053290d9a93fa4910f452e9f8df
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-03-11 02:09:47 +0800

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+if (true) {
+    const companyName = 'Scuber';
+}
+
+
+const mostProfitableNeighborhood = 'Chelsea';
+if (true){let mostProfitableNeighborhood = 'Chelsea';}
+var companyCeo = 'Susan Smith';
+if (true){let companyCeo = 'Susan Smith';
+}
